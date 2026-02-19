896685dfb720c8ca6425229dfc34f04ec5350ca4
Bailey Whitbread <98004245+baileywhitbread@users.noreply.github.com>
2026-02-19 13:42:50 +1100

    Removed some functions
    BraidStacks.jl supersedes most of this

--- src/Bruhat.jl
+++ src/Bruhat.jl
@@ -5,12 +5,9 @@ using Reexport
 @reexport using Logging
 @reexport using Random
 
-include("checks.jl")
 include("intersections.jl")
 
 export intersections_rational
 export intersections_geometric
-export intersections_geometric_ordered
-export verify_lusztig
 
 end
